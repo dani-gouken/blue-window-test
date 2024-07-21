8e0ceae25bb4c2d1568cdc99a232b7fe86a5e2a1
Nghokeng Daniel <nghokend@gmail.com>
2024-07-21 10:40:06 +0100

    feat(backend): brand crud

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class BrandController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        return BrandResource::collection(Brand::latest()->get());
+    }
+
+    public function store(StoreBrandRequest $request): BrandResource
+    {
+        $brand = new Brand;
+        $brand->fill($request->safe()->all());
+        $brand->save();
+        return new BrandResource($brand);
+    }
+
+    public function show(Brand $brand)
+    {
+        return new BrandResource($brand);
+    }
+
+    public function update(StoreBrandRequest $request, Brand $brand): BrandResource
+    {
+        $brand->fill($request->safe()->all());
+        $brand->save();
+        return new BrandResource($brand);
+    }
+
+    public function destroy(Brand $brand): Response
+    {
+        $brand->delete();
+        return response()->make();
+    }
+}
---
+++ app/Models/Brand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    protected $fillable = [
+        'brand_name',
+        'brand_image',
+        'rating',
+    ];
+    
+    protected $casts = [
+        'rating' => 'integer'
+    ];
+
+    use HasFactory;
+}
